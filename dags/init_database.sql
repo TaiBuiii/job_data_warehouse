e605b32dca0b351cf05675343f2e5c0b133d68d1
TaiBuiii <huutaiii2410@gmail.com>
2025-10-05 09:29:37 +0700

    Update init_database.sql

--- dags/init_database.sql
+++ dags/init_database.sql
@@ -1,14 +1,11 @@
--- Đang kết nối vào postgres
 \c postgres;
 
--- Drop và tạo lại DB
 DROP DATABASE IF EXISTS job_datawarehouse;
 CREATE DATABASE job_datawarehouse;
 
--- Kết nối vào DB mới
 \c job_datawarehouse;
 
--- Tạo schema
 CREATE SCHEMA bronze;
 CREATE SCHEMA silver;
 CREATE SCHEMA gold;
+
